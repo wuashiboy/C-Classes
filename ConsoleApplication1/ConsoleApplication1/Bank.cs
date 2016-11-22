10431de8d08aa17b303a53bd1dab77a5eec42467
Peter Le <Peter Le>
2016-11-21 23:26:33 -0500

    Bank commit

--- ConsoleApplication1/ConsoleApplication1/Bank.cs
+++ ConsoleApplication1/ConsoleApplication1/Bank.cs
@@ -6,7 +6,84 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Bank
+    public class Bank
     {
+        public string Name { get; set; }
+        public string BldType { get; set; }
+        public string ConstructionMaterial { get; set; }
+        public int NumberOfRoom { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public double squarefootage
+        {
+            get { return Length * Width; }
+        }
+
+        public int Floors { get; set; }
+        public int Bathrooms { get; set; }
+        public double Volume
+        {
+            get { return Length * Width * Height; }
+        }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+
+        public string FullAddress
+        {
+            get { return ($" {Address1}, {Address2},  {City}, {State}, {Zip}"); }
+        }
+
+        public int Occupants
+        {
+            get { return Occupants; }
+
+        }
+        private double AmountOfMoney { get; set; }
+
+        public Bank()
+        {
+
+            this.Name = Name;
+            this.BldType = BldType;
+            this.NumberOfRoom = 0;
+            this.Height = 0;
+            this.Width = 0;
+            this.Length = 0;
+            this.Floors = 0;
+            this.Bathrooms = 0;
+            this.Address1 = Address1;
+            this.Address2 = Address2;
+            this.City = City;
+            this.State = State;
+            this.Zip = Zip;
+            this.AmountOfMoney = 0;
+
+
+        }
+        public static int IncreaseOccupants(int CurrentOccupants)
+        {
+            return CurrentOccupants++;
+        }
+
+        public static int DecreaseOccupants(int CurrentOccupants)
+        {
+            return CurrentOccupants--;
+        }
+
+        
+        public static double DepositeMoney(double AmountOfMoney, double balance)
+        {
+            return balance + AmountOfMoney;
+        }
+
+        public static double WithdrawMoney(double AmountOfMoney, double balance)
+        {
+            return balance - AmountOfMoney;
+        }
     }
+
 }
