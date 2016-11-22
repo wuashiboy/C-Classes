801f894ab814d9abe7f474b2bcf1b9afd97b5447
Peter Le <Peter Le>
2016-11-21 22:58:33 -0500

    house commit

---
+++ ConsoleApplication1/ConsoleApplication1/House.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class House
+    {
+        public string Name { get; set; }
+        public string BldType {get; set;}
+        public string ConstructionMaterial { get; set; }
+        public int NumberOfRoom { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public double squarefootage
+        {
+            get { return Length * Width; }
+        }
+
+        public int Floors { get; set; }
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public double Volume
+        {
+            get { return Length * Width * Height; }
+        }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+
+        public string FullAddress
+        {
+            get { return ($" {Address1}, {Address2},  {City}, {State}, {Zip}"); }
+        }
+
+        public int Occupants
+        {
+            get { return Occupants; }
+            
+        }
+
+        public House()
+    {
+
+            this.Name = Name;
+            this.BldType = BldType;
+            this.NumberOfRoom = 0;
+            this.Height = 0;
+            this.Width = 0;
+            this.Length = 0;
+            this.Floors = 0;
+            this.Bedrooms = 0;
+            this.Bathrooms = 0;
+            this.Address1 = Address1;
+            this.Address2 = Address2;
+            this.City = City;
+            this.State = State;
+            this.Zip = Zip;
+            
+                     
+    }
+        public static int IncreaseOccupants (int CurrentOccupants)
+        {
+            return CurrentOccupants++;
+        }
+
+        public static int DecreaseOccupants(int CurrentOccupants)
+        {
+            return CurrentOccupants--;
+        }
+
+
+
+    }
+
+
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class TestHouse
+    {
+        static void Main(string[] args)
+        {
+            House house = new House();
+
+            house.BldType = "single family";
+            string housetype = house.BldType;
+
+            house.Name = "mi casa";
+            string waffle = house.Name;
+
+
+            Console.WriteLine($"{housetype} : {waffle}");
+            Console.ReadLine();
+        }
+    }
+
+}
